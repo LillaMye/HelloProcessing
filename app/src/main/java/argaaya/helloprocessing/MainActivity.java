42a3f30a6ff555b3c7145cf496bf02e2b0f4351f
LillaMye <mpalmqvist@msn.com>
2016-08-29 19:37:37 +0200

    Added android studio project

---
+++ app/src/main/java/argaaya/helloprocessing/MainActivity.java
@@ -0,0 +1,24 @@
+package argaaya.helloprocessing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*--- new ---*/
+import android.app.Fragment;
+import android.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*--- new ---*/
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment fragment = new text();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
+}
